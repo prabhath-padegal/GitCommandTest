4764a2e6c4b093a2e3df5718a8c9a3d782ff094a
prabhath <padegalp@gmail.com>
2019-05-11 17:42:59 +0530

    Test data for cmd1 in workspace

--- GITTest/src/com/testing/TestProject.java
+++ GITTest/src/com/testing/TestProject.java
@@ -4,7 +4,7 @@ public class TestProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test data for cmd1");
+		System.out.println("Test data for cmd1 in workspace");
 	}
 
 }

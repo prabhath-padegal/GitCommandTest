2486a82819706390150c46fd85cf6e42f71f6ed1
prabhath <padegalp@gmail.com>
2019-05-11 17:51:02 +0530

    Test data for cmd1 workspace3

--- GITTest/src/com/testing/TestProject.java
+++ GITTest/src/com/testing/TestProject.java
@@ -4,7 +4,8 @@ public class TestProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test data for cmd1");
+		System.out.println("Test data for cmd1 workspace3");
+		System.out.println("Test data for cmd1 merging and fetching");
 	}
 
 }
